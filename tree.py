15e2b0af22f19589d2588e05478ecbd682275dcb
Robert Młynarczyk <48297993+HexosR@users.noreply.github.com>
2019-10-29 23:48:11 +0000

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,9 @@
+def tree(x ,y):
+        tree1 = x.center(7, y)
+        tree2 = (3 * x).center(7, y)
+        tree3 = (5 * x).center(7, y)
+        tree4 = (7 * x).center(7, y)
+        tree5 = x.center(7, y)
+        tree6 = x.center(7, y)
+        print (tree1 + '\n' + tree2 + '\n' + tree3 + '\n' + tree4 + '\n' + tree5 + '\n' + tree6)
+    
